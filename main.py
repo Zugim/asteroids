c0837b6c15e87c73754374c4cabc032474610799
Joe Reeve <joe.reeve@gmail.com>
2025-09-28 20:01:27 +0900

    set 60fps limit

--- main.py
+++ main.py
@@ -9,13 +9,18 @@ def main():
     screen = pygame.display.set_mode(
         (constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        screen.fill(constants.BLACK)
+        screen.fill("black")
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
